2802250f0208bb1f2f54ffa75492d76058816d7e
Jinshow88 <ljun88@naver.com>
2025-11-28 00:10:41 +0900

    시작?

---
+++ wer/src/Header.js
@@ -0,0 +1,18 @@
+
+function Header() {
+  return (
+    <div>
+        <button>새소식</button>
+        <button>게임소개</button>
+        <button>가이드</button>
+<button>모험가월드</button>
+<button>커뮤니티</button>
+<button>창작콘텐츠</button>
+<button>서비스센터</button>
+<button>자료실</button>
+<button>던파ON</button>
+    </div>
+  )
+}
+
+export default Header
